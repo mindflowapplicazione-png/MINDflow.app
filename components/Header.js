ff8c817e3c980ec4233c24d91398d5dbb5536761
MINDflow <mindflow.applicazione@gmail.com>
2025-08-23 23:41:20 +0200

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){ 
+  return (
+    <div className="panel">
+      <h1 style={{color:"var(--accent)"}}>MINDflow</h1>
+      <div className="small">Il tuo viaggio verso la consapevolezza</div>
+    </div>
+  );
+}
