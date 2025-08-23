f303a18353953832c93cac4dcdb8db39671837ab
MINDflow <mindflow.applicazione@gmail.com>
2025-08-23 23:33:46 +0200

    Delete app directory

--- app/success/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Success(){
-  return (
-    <div className="panel">
-      <h2>✅ Pagamento riuscito</h2>
-      <p>Grazie! Il tuo account verrà aggiornato a breve.</p>
-    </div>
-  );
-}
