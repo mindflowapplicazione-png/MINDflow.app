3610580a809c6b0da084963de73f2482bf507719
MINDflow <mindflow.applicazione@gmail.com>
2025-08-23 23:34:46 +0200

    Delete components directory

--- components/TimeRange.jsx
+++
@@ -1,8 +0,0 @@
-export default function TimeRange({ start, end, onChange }) {
-  return (
-    <div className="row">
-      <input type="time" className="input" value={start} onChange={e=>onChange(e.target.value, end)} />
-      <input type="time" className="input" value={end} onChange={e=>onChange(start, e.target.value)} />
-    </div>
-  );
-}
