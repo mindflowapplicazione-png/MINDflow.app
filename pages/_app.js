d110269e3c8ddc864da1c920b1c99e9fad728858
MINDflow <mindflow.applicazione@gmail.com>
2025-08-25 00:48:48 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
 // pages/_app.js
 import { StoreProvider } from '../lib/store';
-
 export default function App({ Component, pageProps }) {
-  return (
-    <StoreProvider>
-      <Component {...pageProps} />
-    </StoreProvider>
-  );
+  return (<StoreProvider><Component {...pageProps} /></StoreProvider>);
 }
